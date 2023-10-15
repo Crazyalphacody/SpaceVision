c8463c8b168516087f868207a2eacd7e92206c26
devprince <justinshcaffer0402@gmail.com>
2023-10-14 19:30:27 -0700

    commit

--- spacevision_backend/server/settings.py
+++ spacevision_backend/server/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+#commit
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-5#oaiptb&98m=a^*dqn!g9u_4bb0bvqj!hkdh7h6k!y$lava-*'
